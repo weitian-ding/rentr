4e17299fcc26d0a2c2553845d58b482d5f0092df
Allen <allendinghamilton@gmail.com>
2016-09-16 00:22:56 -0400

    added resources

---
+++ resources/Square-Admin-Full-Package v3.0/square/client/app/app.module.js
@@ -0,0 +1,42 @@
+(function () {
+    'use strict';
+
+    angular.module('app', [
+        // Angular modules
+        'ngRoute',
+        'ngAnimate',
+        'ngAria',
+
+        // 3rd Party Modules
+        'ui.bootstrap',
+        'ui.tree',
+        'ngMap',
+        'ngTagsInput',
+        'textAngular',
+        'angular-loading-bar',
+        'ui.calendar',
+        'duScroll',
+        'mgo-angular-wizard',
+
+        // Custom modules
+        'app.nav',
+        'app.i18n',
+        'app.chart',
+        'app.ui',
+        'app.ui.form',
+        'app.ui.form.validation',
+        'app.page',
+        'app.table',
+        'app.task',
+        'app.calendar'
+    ]);
+
+})();
+
+
+
+
+
+
+    
+
