0a58abb5d803c17d7cfcbf69bf45a4295b9b1a44
Allen <allendinghamilton@gmail.com>
2017-05-27 00:29:40 -0400

    added dummy routing

--- client/app/layout/layout.component.js
+++ client/app/layout/layout.component.js
@@ -4,7 +4,15 @@
     angular.module('app').component('layout', {
         templateUrl: '/app/layout/layout.html',
         controller: function($scope, sessionService) {
-            $scope.first_name = sessionService.get_first_name()
+            $scope.first_name = sessionService.get_first_name();
+
+            var originatorEv;
+
+            this.openMenu = function($mdMenu, ev) {
+                originatorEv = ev;
+                $mdMenu.open(ev);
+            };
+
         }
     });
 
