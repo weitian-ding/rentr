2be73c49392198377141f214f8b7b57bce237de2
Allen <allendinghamilton@gmail.com>
2017-06-29 21:42:21 -0400

    updated post controller

--- routes/post.js
+++ routes/post.js
@@ -2,19 +2,53 @@
  * Created by Allen on 2017-05-25.
  */
 
-// dummy post controller
 
 var express = require('express');
 var router = express.Router();
 var multer = require('../services/multer');
 var path = require('path');
+var models = require('../models');
+var bookshelf = require('../services/bookshelf');
 
 
-// route to test if the user is logged in or not
-router.post('/', multer.array('photos', 12), function (req, res) {
-    // console.log(req);
-    res.end('got you');
+router.post('/', multer.array('photos', 12), function (req, res, next) {
+    // the transaction to post a new property
+    var post_property_transac = function(t) {
+        // create a new entry in address table
+        return new models.Address({
+            lat: req.body.addr.lat,
+            lng: req.body.addr.lng,
+            city_id: 1  // Toronto
+        })
+            .save(null, { transaction: t })
+            .then(function (address) {
+                // expiry date of a post is two weeks from it is created
+                var expiry_date = new Date();
+                expiry_date.setDate(expiry_date.getDate() + 14);
 
+                // create a new entry in property table
+                return new models.Property({
+                    create_datetime: new Date(),
+                    expiry_datetime: expiry_date,
+                    availability_start_date: new Date(req.body.availability.from),
+                    availability_end_date: new Date(req.body.availability.to),
+                    address_id: address.id,
+                    property_type_id: 2, // apartment
+                    owner: req.user.id
+                })
+                    .save(null, {transaction: t})
+            })
+    };
+
+    bookshelf.transaction(post_property_transac)
+        .then(function(property) {
+            res.send(property.id);
+        })
+        .catch(function(err) {
+            console.log(err);
+            res.sendStatus(500);
+            res.end();
+        });
 });
 
 
--- services/bookshelf.js
+++ services/bookshelf.js
@@ -2,4 +2,6 @@ var dbconfig = require('../knexfile.js');
 var knex = require('knex')(dbconfig.development);
 var bookshelf = require('bookshelf')(knex);
 
+bookshelf.plugin('registry');
+
 module.exports = bookshelf;
