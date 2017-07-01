cd6868f176ddbd64108d2dd8f6314c5b1530d3cd
Allen <allendinghamilton@gmail.com>
2017-07-01 15:36:30 -0400

    new libs added

--- models/index.js
+++ models/index.js
@@ -2,10 +2,12 @@ var User = require("./user");
 var Property = require("./property");
 var Address = require('./address');
 var City = require('./city');
+var Photo = require('./photo');
 
 module.exports = {
     User: User,
     Property: Property,
     Address: Address,
-    City: City
+    City: City,
+    Photo: Photo
 };
